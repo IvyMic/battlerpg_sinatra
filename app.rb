967f6a120f07dfbfb7fabf0da5f32210358223b9
Ibra <ibra@MacBook-Pro-6.local>
2018-09-19 11:33:30 +0100

    refactored POST request to use POST-redirect loop

--- app.rb
+++ app.rb
@@ -1,14 +1,21 @@
 require "sinatra/base"
 
 class Battle < Sinatra::Application
+  enable :sessions
   get "/" do
     'Testing infrastructure working!'
     erb(:index)
   end
 
   post "/names" do
-    @player1_name = params[:player1_name]
-    @player2_name = params[:player2_name]
+    session[:player1_name] = params[:player1_name]
+    session[:player2_name] = params[:player2_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player1_name = session[:player1_name]
+    @player2_name = session[:player2_name]
     erb(:play)
   end
 
