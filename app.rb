952187648a9819d240b0b0082815187136ecee3f
Ibra <ibra@MacBook-Pro-6.local>
2018-09-20 16:42:20 +0100

    refactored t remove global variables

--- app.rb
+++ app.rb
@@ -1,41 +1,43 @@
 require "sinatra/base"
 require_relative "./lib/player.rb"
 require_relative "./lib/game.rb"
+
 class Battle < Sinatra::Application
   enable :sessions
-  
-
+  before do
+    @game = Game.instance
+  end
   get "/" do
     'Testing infrastructure working!'
     erb(:index)
   end
 
   post "/names" do
-    $game = Game.new(Player.new(params[:player1_name]), Player.new(params[:player2_name]))
+    @game = Game.create(Player.new(params[:player1_name]), Player.new(params[:player2_name]))
     redirect '/play'
   end
 
   get '/play' do
-    @active_player = $game.active_player
-    @inactive_player = $game.inactive_player
+    @active_player = @game.active_player
+    @inactive_player = @game.inactive_player
     erb(:play)
   end
 
   get '/attack' do
-    @active_player = $game.active_player
-    @inactive_player = $game.inactive_player
-    $game.attack(@inactive_player)
-    redirect '/game_over' if $game.game_over?
+    @active_player = @game.active_player
+    @inactive_player = @game.inactive_player
+    @game.attack(@inactive_player)
+    redirect '/game_over' if @game.game_over?
     erb(:attack)
   end
 
   post '/switch_players' do
-    $game.switch_players
+    @game.switch_players
     redirect '/play'
   end
 
   get "/game_over" do
-    @inactive_player = $game.inactive_player 
+    @inactive_player = @game.inactive_player 
     erb(:game_over)
   end
   
--- lib/game.rb
+++ lib/game.rb
@@ -1,11 +1,19 @@
 
 class Game
-  attr_reader :active_player ,:inactive_player
+  attr_reader :active_player, :inactive_player, :game 
    def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
     @active_player = @player1
     @inactive_player = @player2
+   end
+
+   def self.create(player1, player2)
+    @game = Game.new(player1, player2)
+   end
+
+   def self.instance
+    @game
    end
 
    def attack(attacked_player)
